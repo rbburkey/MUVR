fb6579dcc33d3bee138ac8f3db5875543030a83e
brennomoreira <bhnmoreira@gmail.com>
2016-03-05 12:15:23 -0500

    added css mover migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305153445) do
+ActiveRecord::Schema.define(version: 20160305163953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,18 +25,24 @@ ActiveRecord::Schema.define(version: 20160305153445) do
   end
 
   create_table "movers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "user_name"
+    t.string   "drivers_license"
+    t.string   "service_area"
+    t.boolean  "truck_available",        default: false
   end
 
   add_index "movers", ["email"], name: "index_movers_on_email", unique: true, using: :btree
