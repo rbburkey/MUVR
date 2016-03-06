c4d8d191641c9985e9bd0bec0f1ddb0e7201e752
Nadia Bharose <nadia@Nadias-MacBook-Pro.local>
2016-03-06 12:57:19 -0500

    routes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   before_filter :client_only, only:[:new, :create, :update, :destroy, :edit]
+  before_action :authenticate_user! || :authenticate_mover!, only: [:show, :index]
 
 
 
