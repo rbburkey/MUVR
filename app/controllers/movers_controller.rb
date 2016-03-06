ad5a084703c6ef9665856bd46ab42df375b943a0
rbburkey <robinsonbburkey@gmail.com>
2016-03-06 12:01:23 -0500

    sunday morning merges

--- app/controllers/movers_controller.rb
+++ app/controllers/movers_controller.rb
@@ -1,27 +1,27 @@
 class MoversController < ApplicationController
   # before_action :set_mover, only: [:]
   def portal
-    @mover = Mover.find_by(params[:id])
+    @users = User.all
+    @mover = current_mover
   end
 
   def profile
-
+@mover = current_mover
   end
-
-
-
-
-
-
-private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_mover
-    @mover = mover.find(params[:id])
-  end
-
-  def mover_params
-    params.require(:mover).permit(:first_name, :last_name, :avatar, :user_name, :email, :truck_available, :license, :service_area, :password, :password_confirmation)
->>>>>>> robbie
-  end
-
 end
+
+
+
+
+
+# private
+#   # Use callbacks to share common setup or constraints between actions.
+#   def set_mover
+#     @mover = mover.find(params[:id])
+#   end
+#
+#   def mover_params
+#     params.require(:mover).permit(:first_name, :last_name, :avatar, :user_name, :email, :truck_available, :license, :service_area, :password, :password_confirmation)
+#   end
+#
+# end
