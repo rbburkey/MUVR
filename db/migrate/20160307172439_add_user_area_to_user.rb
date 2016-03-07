ce9a1bbe23e55e206f9d1ac34f634deaad5af32e
rbburkey <robinsonbburkey@gmail.com>
2016-03-07 15:56:32 -0500

    ignore

--- db/migrate/20160307172439_add_user_area_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddUserAreaToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :user_area, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307172439) do
+ActiveRecord::Schema.define(version: 20160306225610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 20160307172439) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
-    t.string   "user_area"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
