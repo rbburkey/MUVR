90cc91cc44e3ba388394b07a5e4449c619c98221
ifoundlina <ifoundlina@gmail.com>
2016-03-07 18:52:48 -0500

    added all movers list page and functions, edited profiles and portals to allow others to view, front end stuff too

--- app/controllers/movers_controller.rb
+++ app/controllers/movers_controller.rb
@@ -14,11 +14,11 @@ before_action :set_mover, only: [:profile]
     @reviews=Review.all
   end
 
-    def jobs_area
-      @jobs = Job.where(area: current_mover.service_area)
-    end
+def all_movers
+  @movers = Mover.all
+end
 
-protected
+private
   def set_mover
     @mover = Mover.find(params[:id])
   end
