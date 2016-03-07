430da7b6e82ae43fb30a84d72d36139e94718e75
ifoundlina <ifoundlina@gmail.com>
2016-03-07 13:33:54 -0500

    stared jobs in you area changes

--- app/controllers/notepad.rb
+++ app/controllers/notepad.rb
@@ -1,39 +1,28 @@
-@mover = Mover.find_by(user_name: params[:user_name])
 
-
-
-get 'movers/:id' => 'movers#profile'
-
-class MoversController < ApplicationController
-  def portal
-    @mover = current_mover
-  end
-
-  def profile
-    @movers = Mover.all
-    @mover = current_mover
-  end
-
-end
-
-class MoversController < ApplicationController
-  def portal
-    @mover = current_mover
-  end
-
-  def profile
-    @mover = Mover.find(params[:id])
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_mover
-      @mover = Mover.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def mover_params
-      params.require(:mover).permit(:first_name, :last_name, :user_name, :email, :truck_available, :license, :service_area)
-    end
-
-end
+<div class="jumbotron">
+  <div class="profile container">
+    <%- model_class = Job -%>
+    <div class="page-header">
+      <h1>Listed Jobs</h1>
+    </div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th><%= model_class.human_attribute_name(:name) %></th>
+          <th><%= model_class.human_attribute_name(:description) %></th>
+          <th><%= model_class.human_attribute_name(:area) %></th>
+          <th><%=t '.actions', :default => t("helpers.actions") %></th>
+        </tr>
+      </thead>
+      <tbody>
+        <% @jobs.each do |job| %>
+        <tr>
+          <td class="orange_button"><%= link_to job.name, job_path(job)%></td>
+          <td><%= job.description %></td>
+          <td><%= job.area %></td>
+        </tr>
+        <% end %>
+      </tbody>
+    </table>
+  </div>
+</div>
