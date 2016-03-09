766e0210d5eaa9105794c9b98f7130b4be25eb73
rbburkey <robinsonbburkey@gmail.com>
2016-03-09 14:15:36 -0500

    map touch ups

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'users/user_apps'
   get 'movers/jobs_area'
-
+  get '/jobs/map' => 'jobs#map'
   resources :jobs do
       resources :jobapps, shallow: true
   end
