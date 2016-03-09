5c499207f62e73c45cc6d26950aea266dc982a54
ifoundlina <ifoundlina@gmail.com>
2016-03-08 20:23:35 -0500

    added map page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -26,6 +26,15 @@ marker.infowindow job.name
 end
 end
 
+def map
+  @jobs = Job.all
+  @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
+marker.lat job.latitude
+marker.lng job.longitude
+marker.infowindow job.name
+ end
+end
+
 
   def client_only
     unless user_signed_in?
