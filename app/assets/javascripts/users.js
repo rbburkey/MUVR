762e7ac8d65996ce16911237260b4d43345f9d23
rbburkey <robinsonbburkey@gmail.com>
2016-03-11 11:05:45 -0500

    minor changes to applications

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,112 @@
-$(function(){
-        $("#jobapp_accepted").click(function(){
-        if($(this).children("input").is(':accepted')){
-          $(this).next().show();
-          }
-        else{
-        $(this).next().hide();
-        }
-      });
-     })
+// "use strict";
+// $( document ).ready(function() {
+//     $("#jobapp_accepted").click(function() {
+//         var isAccepted = $("#jobapp_accepted").prop('checked', true);
+//         var isRejected = $("#jobapp_not_accepted").prop('checked', true);
+//         if(isAccepted) {
+//           console.log("nadiad");
+//             $("#jobapp_not_accepted").attr('checked', false);
+//         }
+//     });
+//
+//     $("#jobapp_not_accepted").click(function() {
+//         var isRejected = $("#jobapp_not_accepted").prop('checked', true);
+//         if(isRejected) {
+//           console.log("nadia");
+//             $("#jobapp_not_accepted").attr('checked', false);
+//         }
+//     });
+// });
+
+
+
+
+// $( document ).ready(function() {
+//   $('#job_accepted').click(function() {
+//         var isChecked = $('[id^=accepted]').prop('checked', true);
+//         if (isChecked) {
+//           $('[id^=accepted]').prop('checked', true);
+//         } else {
+//         $('[id^=accepted]').prop('checked', false);
+//     };
+//   });
+// });
+
+// $( document ).ready(function() {
+//     $("#jobapp_accepted").click(function() {
+//       $("#jobapp_accepted").prop('checked', true);
+//         $("#jobapp_not_accepted").prop('checked', false);
+//       });
+//     });
+// });
+//
+//   $("#jobapp_not_accepted").click(function() {
+//     $("#jobapp_accepted").prop('checked', false);
+//     $("#jobapp_not_accepted").prop('checked', true);
+//   });
+// });
+// 
+// $( document ).ready(function() {
+//   $('#job_accepted').click(function() {
+//         var isChecked = $('[id^=accepted]').prop('checked', true);
+//         if (isChecked) {
+//           $('[id^=accepted]').prop('checked', true);
+//         } else {
+//         $('[id^=accepted]').prop('checked', false);
+//       };
+//     };
+//     });
+//
+// });
+// $( document ).ready(function() {
+//   $('[id^=accepted]').click(function() {
+//         $('[id^=accepted]').prop('checked', true);
+//         $('[id^=not_accepted]').prop('checked', false);
+//     });
+//     $('[id^=not_accepted]').click(function() {
+//           $('[id^=accepted]').prop('checked', false);
+//             $('[id^=not_accepted]').prop('checked', true);
+//     });
+// });
+
+
+// $("#jobapp_not_accepted").click(function() {
+//     var isDisabled = $("#jobapp_not_accepted").prop('checked', false);
+//     if(isDisabled) {
+//       console.log("nadia");
+//         $("#jobapp_accepted").attr('checked', true);
+//     } else {
+//       console.log("jose");
+//         $("#jobapp_not_accepted").prop('checked', false);
+//     }
+// });
+// });
+
+// $('#myCheckbox').prop('checked', true); // Checks it
+// $('#myCheckbox').prop('checked', false); // Unchecks it
+
+
+
+
+// $(function(){
+//   $('Update Jobapp').click(function(){
+//     console.log("hi");
+//     $('.edit_jobapp').hide();
+//   });
+// });
+
+
+
+// $(function(){
+//         $("#accepted_Accept").click(function(){
+//         if($(this).value(":acce").is(':accepted')){
+//           $(this).next().show();
+//           }
+//         else{
+//         $(this).next().hide();
+//         }
+//       });
+//      })
 
  //     $(function(){
  //       $("#check_checkbox").click(function(){
