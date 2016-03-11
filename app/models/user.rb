67398b445b50d4577e984f37da12c77e3516ce5f
Nadia <nadiadevi93@gmail.com>
2016-03-11 16:18:48 -0500

    added conversations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :jobapps, dependent: :destroy
 
+  # for messsaging
+  acts_as_messageable
 
   has_attached_file :avatar, styles: {:medium => "300x300>", :thumb => "100x100>"} #:path =>'controllers/user/registrations_controller.rb'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
