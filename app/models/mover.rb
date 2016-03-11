bdd963a1ec5ebcb113b3d106c7b538ea9dcc504f
Nadia <nadiadevi93@gmail.com>
2016-03-11 16:14:14 -0500

    changed models for messaging

--- app/models/mover.rb
+++ app/models/mover.rb
@@ -9,6 +9,9 @@ class Mover < ActiveRecord::Base
   has_many :reviews
   has_many :jobapps
 
+# for messaging
+acts_as_messageable
+
   has_attached_file :avatar, styles: {:medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
