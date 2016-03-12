4bb824e520be501c93bf287aad35c43f098f1c0a
Nadia <nadiadevi93@gmail.com>
2016-03-12 18:23:23 -0500

    movers are messageable

--- app/models/mover.rb
+++ app/models/mover.rb
@@ -11,6 +11,20 @@ class Mover < ActiveRecord::Base
 
 # for messaging
 acts_as_messageable
+#Returning any kind of identification you want for the model
+def name
+user_name
+end
+
+#Returning the email address of the model if an email should be sent for this object (Message or Notification).
+#If no mail has to be sent, return nil.
+def mailboxer_email(object)
+  #Check if an email should be sent for that object
+  #if true
+  return email
+  #if false
+  #return nil
+end
 
   has_attached_file :avatar, styles: {:medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
