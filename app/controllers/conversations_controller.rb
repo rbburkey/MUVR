2c6044662585b42269a9f4d4fcb9f57f201e94a7
Nadia <nadiadevi93@gmail.com>
2016-03-14 10:58:06 -0400

    fixed messaging

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,8 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user! || :authenticate_mover!
   before_action :get_mailbox
   before_action :get_conversation, except: [:index]
   before_action :get_box, only: [:index]
+
 
 
   def index
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,28 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user! || :authenticate_mover!
+
 
   def new
+    if user_signed_in?
     @user = User.find_by_username(params[:user])
 @message = current_user.messages.new
+else
+  @mover = Mover.find_by_user_name(params[:user])
+@message = current_mover.messages.new
   end
+end
 
   def create
-    recipients = User.where(id: params['recipients']) || Mover.where(id: params['recipients'])
-
+    if user_signed_in?
+    recipients = Mover.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
     flash[:success] = "Message has been sent!"
     redirect_to conversation_path(conversation)
+
+  else
+    recipients = User.where(id: params['recipients'])
+    conversation = current_mover.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
+    flash[:success] = "Message has been sent!"
+    redirect_to conversation_path(conversation)
   end
+end
 end
